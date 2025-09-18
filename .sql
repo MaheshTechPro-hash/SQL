768c308355306a6aef3f291da6d9552a77137483
MaheshTechPro-hash <maheshpalave.tech@gmail.com>
2025-09-18 15:16:12 +0530

    Update .sql

--- .sql
+++ .sql
@@ -1,1 +1,21 @@
+-- Create Database
+CREATE DATABASE student_db;
 
+USE student_db;
+
+-- Create Table
+CREATE TABLE students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    course VARCHAR(50),
+    marks INT
+);
+
+-- Insert Sample Data
+INSERT INTO students (name, course, marks) VALUES
+('Mahesh', 'Computer', 85),
+('Sneha', 'IT', 90),
+('Ravi', 'Mechanical', 75);
+
+-- Select Data
+SELECT * FROM students;
