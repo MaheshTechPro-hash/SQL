02ba85bb8edc84e97a6b84c2603c7833b2fedaf2
MaheshTechPro-hash <maheshpalave.tech@gmail.com>
2025-09-24 23:02:33 +0530

    Add files via upload

---
+++ Scaler Function.sql
@@ -0,0 +1,40 @@
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    StudentName NVARCHAR(50),
+    Marks INT
+);
+
+INSERT INTO Students VALUES
+(1, 'Rahul', 85),
+(2, 'Sneha', 72),
+(3, 'Amit', 90),
+(4, 'Priya', 65);
+
+
+CREATE FUNCTION dbo.fn_StudentsBonusMSTVF()
+RETURNS @Result TABLE
+(
+    StudentID INT,
+    StudentName NVARCHAR(50),
+    Marks INT,
+    MarksWithBonus INT,
+    HighScorer BIT
+)
+AS
+BEGIN
+    -- Insert rows into table variable
+    INSERT INTO @Result
+    SELECT 
+        StudentID,
+        StudentName,
+        Marks,
+        Marks + 5 AS MarksWithBonus,
+        CASE WHEN Marks + 5 >= 90 THEN 1 ELSE 0 END AS HighScorer
+    FROM Students;
+
+    RETURN;
+END;
+
+
+SELECT * FROM dbo.fn_StudentsBonusMSTVF();
+
